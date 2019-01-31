a7ad78322ca4c1bc91a3ac1d6c81c16466d6de91
Sharon.Fediuk <sharonfaduck@gmail.com>
2019-01-31 15:28:33 +0200

    checkdfg

--- src/com/selenium/Main.java
+++ src/com/selenium/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    System.out.println("jtrrrrrddde33fft");
+    System.out.println("jtrrrrrdhhdde33fft");
     }
 }
